a251bca4eca95c1b4f7df061db65f70dd3aa9fe7
Hyerin Lee <hyerinlee0v0@gmail.com>
2023-07-19 17:43:24 +0900

    Day01

--- src/test/java/com/mega/it/springboot/web/HelloControllerTest.java
+++ src/test/java/com/mega/it/springboot/web/HelloControllerTest.java
@@ -1,5 +1,6 @@
 package com.mega.it.springboot.web;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,19 @@ public class HelloControllerTest {
         mvc.perform(MockMvcRequestBuilders.get("/hello"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string(hello));
+    }
+
+    @Test
+    public void helloDtoReturn() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello/dto").param("name", name)  
+                .param("amount", String.valueOf(amount)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.is(name)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.amount", Matchers.is(amount)));
+                //$를 기준으로 필드명을 명시 -> 값을 가져올 때 사용
+                //name과 amount $.name, $.amout
     }
 }
